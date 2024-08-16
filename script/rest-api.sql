789b02b96a2e82f77a9c219b0173520a0f8be56e
ezequiel <ezequiel.mdg@gmail.com>
2024-08-16 20:33:56 -0300

    crea carpeta “script” con el código sql para generar el modelo de datos

---
+++ script/rest-api.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-08-2024 a las 01:30:30
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `rest-api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `autor` varchar(30) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `año` date NOT NULL,
+  `ISBN` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`id`, `nombre`, `autor`, `categoria`, `año`, `ISBN`) VALUES
+(1, 'El Arte de la Guerra', 'Sun Tzu', 'Estrategia', '2020-08-01', '9780132350222'),
+(2, 'Libro El Camino Del Artista', 'Julia Cameron', 'Autoayuda', '2016-01-01', '0978987735230'),
+(3, 'Clean Code', 'Robert C. Martin', 'Programación', '2009-01-01', '9780132350884');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
