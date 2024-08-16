bf42d38ea7c86fa5b3405e675d4908615d366145
ezequiel <ezequiel.mdg@gmail.com>
2024-08-15 21:37:16 -0300

    Modificaciones para adaptar el proyecto al modelo requerido de libros

--- src/controller.js
+++ src/controller.js
@@ -1,29 +1,28 @@
 import {pool} from './database.js';
 
-class PersonaController {
+class LibroController {
     async getAll(req, res) {
-        const [result] = await pool.query('SELECT * FROM personas');
+        const [result] = await pool.query('SELECT * FROM libros');
         res.json(result);
-
     }
 
     async add(req, res) {
-        const persona = req.body;
-        const [result] = await pool.query(`INSERT INTO Personas(nombre, apellido, dni) VALUES (?, ?, ?)`, [persona.nombre, persona.apellido, persona.dni]);
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, año-publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año-publicacion, libro.isbn]);
         res.json({"Id insertado": result.insertId});
     }
 
     async delete(req,res){
-        const persona = req.body;
-        const [result] = await pool.query(`DELETE FROM Personas WHERE id=(?)`,[persona.id]);
+        const libro = req.body;
+        const [result] = await pool.query(`DELETE FROM Libros WHERE id=(?)`,[libro.id]);
         res.json({"Registros eliminados": result.affectedRows});
     }
 
     async update(req,res){
-        const persona = req.body;
-        const [result] = await pool.query(`UPDATE Personas SET nombre=(?), apellido=(?), dni=(?) WHERE id=(?)`, [persona.nombre, persona.apellido, persona.dni, persona.id]);
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año-publicacion=(?), isbn(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año-publicacion, libro.id]);
         res.json({"Registros actualizados": result.changedRows});
     }
 }
 
-export const persona = new PersonaController();
+export const libro = new LibroController();
